0a81a4a48295c85f23d1c3c17399624c88422799
Fabian David <iamfabiandavid@Fabians-MacBook-Air.local>
2026-05-20 20:00:24 +0100

    chore: nightly auto-backup 2026-05-20

--- bpt-academy/mobile/ngrok-wrapper.sh
+++ bpt-academy/mobile/ngrok-wrapper.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # Wrapper: intercepts @expo/ngrok calls and routes to ngrok v3 with static domain
-# Expo calls: ngrok http --log=stdout --authtoken=xxx 8081
-# We replace with: ngrok v3 http 8081 --domain=bpt-academy.ngrok.app
-
-exec /opt/homebrew/bin/ngrok http 8081 \
+exec /opt/homebrew/bin/ngrok http 8083 \
   --domain=bpt-academy.ngrok.app \
   --log=stdout "$@"
